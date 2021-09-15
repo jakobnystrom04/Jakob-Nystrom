4a5d0132c01f0267fd64d76c29119f919cf430b2
jakobnystrom04 <jakob.nystrom@elev.ga.lbs.se>
2021-09-15 18:56:38 +0200

    Environment

--- FPS/Assets/MouseLook.cs
+++ FPS/Assets/MouseLook.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class MouseLook : MonoBehaviour
 {
     public Transform playerBody;
-    public float mouseSensitivity = 100f;
+    public float mouseSensitivity = 400f;
     float xRotation = 0f;
     // Start is called before the first frame update
     void Start()
