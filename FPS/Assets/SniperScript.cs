941005d00c5eb04522778087b9739cf32c630ed4
jakobnystrom04 <jakob.nystrom@elev.ga.lbs.se>
2021-10-13 12:47:31 +0200

    SniperScope

---
+++ FPS/Assets/SniperScript.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class SniperScript : MonoBehaviour
+{
+    public float damage = 0f;
+    public float range = 100f;
+    public float fireRate = 2f;
+   
+
+    public Camera fpsCam;
+    public ParticleSystem muzzleFlash;
+    public GameObject impactEffect;
+    public float impactForce = 30f;
+
+    private float nextTimeToFire = 0f;
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") &&  Time.time > nextTimeToFire)
+        {
+            Shoot();
+            nextTimeToFire = Time.time + fireRate;
+        }
+
+        
+
+
+    }
+
+    void Shoot()
+    {
+        muzzleFlash.Play();
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+
+            if (hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+
+            GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, 1f);
+        }
+    }
+}
