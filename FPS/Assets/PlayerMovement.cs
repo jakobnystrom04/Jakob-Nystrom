6c6d45927dde2440144247e039e6d5c70048d59b
jakobnystrom04 <jakob.nystrom@elev.ga.lbs.se>
2022-03-31 14:22:17 +0200

    Crouch funktion

--- FPS/Assets/PlayerMovement.cs
+++ FPS/Assets/PlayerMovement.cs
@@ -4,23 +4,40 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    
     public CharacterController controller;
 
     public float speed = 12f;
-    public float gravity = -9.81f;
+    readonly float gravity = -9.81f;
     public float jumpHeight = 3f;
+    public float bastianHeight;
+    float normalHeight;
 
     Vector3 velocity;
 
     public Transform GroundCheck;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
+    CharacterController playerCtrl;
+
+    
 
     bool isGrounded;
+
     
+    public float CoolDown = 3;
+
     // Update is called once per frame
+
+    private void Start()
+    {
+        playerCtrl = GetComponent<CharacterController>();
+        normalHeight = playerCtrl.height;
+
+    }
     void Update()
     {
+        
         //Detta skriptet, kollar ifall du står på marken, sedan sätter den så att man inte accelererar neråt, den skaffar z-axel och x-axel, så att man sedan kan flytta längs de axlarna, den sätter sedan in en jump-funktion för att kunna hoppa och sedan falla.
 
         isGrounded = Physics.CheckSphere(GroundCheck.position, groundDistance, groundMask);
@@ -36,13 +53,43 @@ public class PlayerMovement : MonoBehaviour
 
         controller.Move(move * speed * Time.deltaTime);
 
-        //if(Input.GetButtonDown("Jump") && isGrounded)
+        if(Input.GetButtonDown("Jump") && isGrounded)
         {
-            //velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
         }
 
         velocity.y += gravity * Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            speed = 160000000f;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            speed = 12f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            speed = 4f;
+            Bastian();
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            speed = 12f;
+            UnBastian();
+        }
+    }
+    void Bastian()
+    {
+        playerCtrl.height = bastianHeight;
+    }
+
+    void UnBastian()
+    {
+        playerCtrl.height = normalHeight;
     }
 }
---
+++ FPS/Assets/lamoBastian.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LamoBastian : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            Bastian();
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            UnBastian();
+        }
+    }
+
+    void Bastian()
+    {
+        
+        transform.position = transform.position + new Vector3(0, -3 / 4, 0);
+        transform.localScale = new Vector3(1.2f, 0.75f, 1.2f);
+    }
+
+    void UnBastian()
+    {
+        
+        transform.position = transform.position + new Vector3(0, 3 / 4, 0);
+        transform.localScale = new Vector3(1.2f, 1.5f, 1.2f);
+    }
+}
